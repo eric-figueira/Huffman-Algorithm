85bed33def454fb438eca59933e803cf86f8e8f4
Marcos-Godinho-Filho <mcvsk.filho@gmail.com>
2023-11-22 15:00:15 -0300

    priority queue (almost) done

--- PriorityQueue/PriorityQueue.h
+++ PriorityQueue/PriorityQueue.h
@@ -1,8 +1,24 @@
 #ifndef PRIORITYQUEUE
 #define PRIORITYQUEUE
 
-class PriorityQueue {
+#include "LinkedList.h"
 
+template <typename Data>
+class PriorityQueue {
+  private:
+    LinkedList<Data> priorityQueue;
+    unsigned int size;
+
+  public:
+    PriorityQueue();
+
+    LinkedList<Data> get_priorityQueue() const;
+    unsigned int get_size() const;
+    ListNode<Data> get_begin() const;
+    ListNode<Data> get_end() const;
+
+    void enqueue(Data data);
+    void dequeue();
 };
 
 #endif 
