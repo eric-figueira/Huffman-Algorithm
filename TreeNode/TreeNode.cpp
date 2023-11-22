98de05268713f588a70f052606c2de66baf4da02
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-11-22 08:56:49 -0300

    feat: add classes

---
+++ TreeNode/TreeNode.cpp
@@ -0,0 +1,38 @@
+#include "TreeNode.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename Data>
+TreeNode<Data>::TreeNode(Data d, TreeNode* l, TreeNode * r) : data(d), left(l), right(r) {
+  if (d == NULL || l == NULL || r == NULL) {
+    cerr << "[TreeNode]: Missing Parameters";
+    exit(-1);
+  }
+}
+
+template <typename Data>
+TreeNode<Data>::TreeNode(Data d) : data(d), left(NULL), right(NULL) {
+  if (d == NULL) {
+    cerr << "[TreeNode]: Data should not be null";
+    exit(-1);
+  }
+}
+
+template <typename Data>
+TreeNode<Data>::TreeNode() : data(NULL), left(NULL), right(NULL) {}
+
+template <typename Data>
+TreeNode<Data>* TreeNode<Data>::get_left() const{
+  return left;
+}
+
+template <typename Data>
+TreeNode<Data>* TreeNode<Data>::get_right() const {
+  return right;
+}
+
+template <typename Data>
+Data TreeNode<Data>::get_data() const {
+  return data;
+}
---
+++ TreeNode/TreeNode.h
@@ -0,0 +1,23 @@
+#ifndef TREENODE
+#define TREENODE
+
+
+template <typename Data>
+class TreeNode
+{
+  private:
+    TreeNode* left;
+    TreeNode* right;
+    Data data;
+  
+  public:
+    TreeNode* get_left()  const;
+    TreeNode* get_right() const;
+    Data      get_data()  const;
+    
+    TreeNode(Data d, TreeNode* l, TreeNode* r);   
+    TreeNode(Data d);
+    TreeNode();
+}
+
+#endif
