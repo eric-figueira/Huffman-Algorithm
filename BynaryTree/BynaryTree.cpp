8a024ee50975908ec82ac8370515d9f489c48b04
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2023-11-23 21:48:27 -0300

    feat: add BynaryTree class

---
+++ BynaryTree/BynaryTree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "BynaryTree.h"
+#include "TreeNode.h"
+
+using namespace std;
+
+template <typename T>
+BynaryTree<T>::BynaryTree() : first(NULL), current(NULL), previous(NULL) {}
+
+template <typename T>
+void BynaryTree<T>::insert_node(T data) {
+  // todo
+}
