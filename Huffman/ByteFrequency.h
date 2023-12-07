3cb1fc92d756c14be34c28d829c098e01fe60428
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2023-12-07 11:58:13 -0300

    unsigned long long frequency

--- Huffman/ByteFrequency.h
+++ Huffman/ByteFrequency.h
@@ -11,20 +11,20 @@ using namespace std;
 class ByteFrequency
 {
     private:
-        unsigned int frequency;
+        unsigned long long frequency;
         byte byte_code;
 
     public:
-        unsigned int get_frequency() const;
+        unsigned long long get_frequency() const;
         byte get_byte_code() const;
 
-        void set_frequency(unsigned int f);
+        void set_frequency(unsigned long long f);
         void set_byte_code(byte b);
         void increment_frequency();
 
         bool is_empty();
 
-        ByteFrequency(unsigned int f, byte b);
+        ByteFrequency(unsigned long long f, byte b);
         ByteFrequency();
 
         friend ofstream& operator<< (ofstream& os, const ByteFrequency& bf);
